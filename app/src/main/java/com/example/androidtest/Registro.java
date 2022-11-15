9ffdfed494bebdead60a85586959798c0ecea387
Tony <j.a_alvarado99@hotmail.com>
2022-11-15 15:59:27 -0600

    Registro con Scroll

--- app/src/main/java/com/example/androidtest/Registro.java
+++ app/src/main/java/com/example/androidtest/Registro.java
@@ -2,18 +2,27 @@ package com.example.androidtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
     EditText nombre, apellidos, contraseña, email, carnet, sede;
+    Button registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
 
+        nombre=(EditText)findViewById(R.id.nombre);
+        apellidos=(EditText)findViewById(R.id.apellidos);
+        email=(EditText)findViewById(R.id.email);
+        contraseña=(EditText)findViewById(R.id.contraseña);
+        carnet=(EditText)findViewById(R.id.carnet);
+
+        registrar=(Button)findViewById(R.id.registrar);
     }
     public void registrar(){
         try{
