6931934ca2a7c592e7b79233b79a13b7d19ffbab
Tony <j.a_alvarado99@hotmail.com>
2022-11-17 14:08:32 -0600

    Corrección login

---
+++ app/src/main/java/com/example/androidtest/Anuncio.java
@@ -0,0 +1,14 @@
+package com.example.androidtest;
+
+public class Anuncio {
+    private String titulo;
+    private String descripcion;
+    private String sede;
+    private String precio;
+    public void anuncio(String titutloP, String contenido, String sedeP, String precioP) {
+        titulo=titutloP;
+        descripcion=contenido;
+        sede=sedeP;
+        precio=precioP;
+    }
+}
---
+++ app/src/main/java/com/example/androidtest/ui/home/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.androidtest.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.androidtest.R;
+import com.example.androidtest.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        String[] test={"Secadora","Gabacha","Escritorio"};
+        ListView lv=root.findViewById(R.id.anuncios);
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1,test);
+
+        lv.setAdapter(adapter);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
