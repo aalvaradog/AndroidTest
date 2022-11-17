9c41f36e2ca9f2659283747bf2ab1eb2e255b449
Tony <j.a_alvarado99@hotmail.com>
2022-11-17 00:10:29 -0600

    - Menú inicio sin adaptador - Bottom navigation bar adaptada al cotexto del proyecto

---
+++ app/src/main/java/com/example/androidtest/Anuncio_adaptador.java
@@ -0,0 +1,13 @@
+package com.example.androidtest;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.List;
+
+public class Anuncio_adaptador {
+
+    public void adaptador(Context contexto, List<Anuncio> listaAnuncios ){
+
+    }
+}
