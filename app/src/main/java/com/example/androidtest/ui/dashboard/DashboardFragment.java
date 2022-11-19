3640fffb4ea6ed57d6e179e00082f1d090457dae
Tony <j.a_alvarado99@hotmail.com>
2022-11-19 00:28:41 -0600

    Cargar imagen desde el fragment para publicar un anuncio

--- app/src/main/java/com/example/androidtest/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/androidtest/ui/dashboard/DashboardFragment.java
@@ -1,13 +1,26 @@
 package com.example.androidtest.ui.dashboard;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,6 +31,8 @@ import com.example.androidtest.databinding.FragmentDashboardBinding;
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    ImageView campoImagen;
+    Button agregarImagen;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +45,9 @@ public class DashboardFragment extends Fragment {
         Spinner categorias=root.findViewById(R.id.categoria);
         Spinner sedes=root.findViewById(R.id.sede);
 
+        campoImagen=(ImageView)root.findViewById(R.id.campoImagen);
+        agregarImagen=(Button)root.findViewById(R.id.AgregarImagen);
+
         String[] test={"Selecione la categoria...","Muebles","Ofimatica","Ropa"};
         String[] test2={"Selecione la sede...","San José","Cartago","Alajuela","San Carlos"};
 
@@ -38,7 +56,37 @@ public class DashboardFragment extends Fragment {
 
         categorias.setAdapter(adapter);
         sedes.setAdapter(adapter2);
+
+        agregarImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cargarImagen();
+                //cameraLauncher.launch(new Intent(MediaStore.ACTION_IMAGE_CAPTURE));
+            }
+        });
+
         return root;
+    }
+
+    ActivityResultLauncher<Intent> cameraLauncher= registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if(result.getResultCode() == RESULT_OK){
+                        Uri data=result.getData().getData();
+                        binding.campoImagen.setImageURI(data);
+                        //Bundle extras=result.getData().getExtras();
+                        //Bitmap imgBitmap=(Bitmap) extras.get("data");
+                        //campoImagen.setImageBitmap(imgBitmap);
+                    }
+                }
+            });
+
+    public void cargarImagen(){
+        Intent intent=new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        cameraLauncher.launch(intent);
     }
 
     @Override
